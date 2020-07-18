cd3ac86e4e3ff2b6c874928aa4c887e23435b033
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-07-18 11:15:10 -0500

    Pruebas para Boolean, Arrays y callbacks

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,7 +1,32 @@
 // la funcion test recibe dos parametros: Un string que describe lo que va a pasar. Función anonima que viene lo que se va a probar
 // La función expect: valor de entrada, sobre que lo voy a comparar usando .toMach()
 const text = "Hola Mundo"
+const fruits = ['manzana', 'melon', 'banana']
 
 test('Debe contener un texto ', () => {
     expect(text).toMatch(/Mundo/);
 });
+
+test('¿Tenemos una banana?', () => {
+    expect(fruits).toContain('banana') //recibe un array y compara si este array contiene el string banana
+})
+
+test('Mayor que', () => {
+    expect(10).toBeGreaterThan(9) //si 10 es mayor que 9 paso el test
+})
+
+test('Es Verdadero?', () => {
+    expect(true).toBeTruthy() //comprueba si el valor es verdadero
+})
+
+
+// funcion que llama al callback y le pasa el string en reverse
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+}
+
+test('Probar un Callback', () => {
+    reverseString("Hola", (str) => {
+        expect(str).toBe('aloH')
+    })
+})
