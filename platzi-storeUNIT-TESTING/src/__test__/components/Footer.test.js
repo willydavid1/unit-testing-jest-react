53aff297b388e84a7a775ef250ade2da7c38895b
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-07-20 17:08:56 -0500

    Crear mocks

--- platzi-storeUNIT-TESTING/src/__test__/components/Footer.test.js
+++ platzi-storeUNIT-TESTING/src/__test__/components/Footer.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme'; //nos permite montar el componente en el dom y trabajar con cada uno de sus items
+import { mount } from 'enzyme'; //nos permite montar el componente en el dom y trabajar con cada uno de sus items ejemplo .find() para encontrar un elemento
 import Footer from '../../components/Footer';
 
 // describe mi suite para este componente
 describe('<Footer />', () => {
+  const footer = mount(<Footer />); // dejamos el footer en el cuerpo de la suite
+
   test('Render del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
